883542e7f468fc1a2c302a6d4143c588295acc7c
YuriFAToledo <yuri.toledo@sou.inteli.edu.br>
2023-09-06 23:51:02 -0300

    commit por 3001 do backend

--- src/backend/backend/src/main.ts
+++ src/backend/backend/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors()); // Adicione esta linha
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
